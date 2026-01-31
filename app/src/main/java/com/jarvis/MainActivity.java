c2d5ddbcd27c23d870b5010199da91c7aa2daccc
Minaty <saif.ali321554@gmail.com>
2026-02-01 00:32:45 +0530

    Add MainActivity for Jarvis App with TextView

--- app/src/main/java/com/jarvis/MainActivity.java
+++ app/src/main/java/com/jarvis/MainActivity.java
@@ -1,1 +1,17 @@
+package com.jarvis;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+
+        TextView t = new TextView(this);
+        t.setText("Jarvis App");
+        setContentView(t);
+    }
+}
